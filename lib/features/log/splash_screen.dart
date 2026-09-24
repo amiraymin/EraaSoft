1e6c602b6d7d5908feae95ba2bf2b2072fb500fb
Amir <amiraymin6066@gmail.com>
2026-09-24 23:25:02 +0300

    add splash screen

---
+++ lib/features/log/splash_screen.dart
@@ -0,0 +1,27 @@
+import 'package:apptask/features/log/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 3),(){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> LoginScreen()));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Lottie.asset('assets/icons/splash.json')),
+    );
+  }
+}
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,4 +1,4 @@
-import 'package:apptask/features/log/login_screen.dart';
+import 'package:apptask/features/log/splash_screen.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
-        home: LoginScreen(),
+        home: SplashScreen(),
       ),
     );
   }
